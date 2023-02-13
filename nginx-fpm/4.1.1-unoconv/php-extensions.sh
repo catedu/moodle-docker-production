0cf2d569f61c8b32eeeb0f4d0e92cc0b11a15a96
Pablo Ruiz Soria <pabloruizsoria@gmail.com>
2023-02-13 11:33:09 +0100

    Update commands of GD extension installation

--- nginx-fpm/4.1.1-unoconv/php-extensions.sh
+++ nginx-fpm/4.1.1-unoconv/php-extensions.sh
@@ -59,8 +59,7 @@ docker-php-ext-install -j$(nproc) \
 
 # GD.
 echo "***** Installing GD"
-# docker-php-ext-configure gd --with-jpeg --with-freetype
-docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
+docker-php-ext-configure gd --with-freetype=/usr/include/ --with-jpeg=/usr/include/
 docker-php-ext-install -j$(nproc) gd
 
 # LDAP.
