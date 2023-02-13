3a8fb7809b35fabe0085b846ee09ebc9018027e9
Pablo Ruiz Soria <pabloruizsoria@gmail.com>
2023-02-13 11:04:02 +0100

    Update GD configuration

--- nginx-fpm/4.1.1-unoconv/php-extensions.sh
+++ nginx-fpm/4.1.1-unoconv/php-extensions.sh
@@ -58,7 +58,6 @@ docker-php-ext-install -j$(nproc) \
 
 # GD.
 echo "***** Installing GD"
-# docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
 # docker-php-ext-configure gd --with-jpeg --with-freetype
 docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
 docker-php-ext-install -j$(nproc) gd
@@ -75,7 +74,7 @@ docker-php-ext-enable memcached mongodb redis apcu igbinary uuid
 
 # ZIP
 echo "***** Installing ZIP"
-# docker-php-ext-configure zip --with-libzip
+docker-php-ext-configure zip --with-libzip
 docker-php-ext-install zip
 
 echo 'apc.enable_cli = On' >> /usr/local/etc/php/conf.d/docker-php-ext-apcu.ini
